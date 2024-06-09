563a823a16cf67d53267332536966bf3a578988b
adityajoy-1902 <aditya44492@gmail.com>
2024-06-09 11:13:11 +0530

    adding to learn to commit from terminal

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"hello World!"<<endl;
+    cout<<"Aditya is doing this shit to check things are working properly"<<endl;
+     vector<int> myVector;
+
+  // Add elements to the vector
+  myVector.push_back(1);
+  myVector.push_back(2);
+  myVector.push_back(3);
+
+  // Print the elements of the vector
+  for (int i = 0; i < myVector.size(); i++) {
+    cout << myVector[i] << " ";
+  }
+
+  cout << endl;
+
+}
